b1b24b4a94abf31c9746c6b53000b617e1fbc626
David Lynam <davel@a-w-s.co.uk>
2021-04-20 14:09:45 +0100

    update images folder

--- inc/header.php
+++ inc/header.php
@@ -100,7 +100,8 @@ if (isset($_GET[ 'bathroom'] )) {
             <!-- Navigation -->
             <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
                 <div class="container">
-                    <a class="navbar-brand" href="../index.php"><h1>Tutto</h1></a>
+                    <!--<a class="navbar-brand" href="../index.php"><h1>Tutto</h1></a>-->
+                    
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto">
